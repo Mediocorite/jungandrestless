ca3886b888249ad34b4a3a4a7129a310b402a4bf
Anzar Ahmad Malik <12586091+Mediocorite@users.noreply.github.com>
2025-09-01 10:46:18 +0100

    Added new blog post and editing posts

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -2,20 +2,29 @@
 import { z } from 'astro:content'
 
 export const CATEGORIES = {
-	'Resources': [],
-	'Research Review': ['Social Psychology', 'Health Psychology', 'Neuropsychology', 'Cognitive Psychology', 'Developmental Psychology', 'Clinical Psychology'],
-	'Ethical Considerations':[],
-	'Personal Reflections': [],
+	Resources: [],
+	'Research Review': [
+		'Social Psychology',
+		'Health Psychology',
+		'Neuropsychology',
+		'Cognitive Psychology',
+		'Developmental Psychology',
+		'Clinical Psychology',
+		'Behavioral Psychology'
+	],
+	'Ethical Considerations': [],
+	'Personal Reflections': []
 } as const
 
-export type CategoriesMap = typeof CATEGORIES;
+export type CategoriesMap = typeof CATEGORIES
 
-const categoryNames = Object.keys(CATEGORIES) as [keyof CategoriesMap, ...(keyof CategoriesMap)[]];
-export const CategoryEnum = z.enum(categoryNames);
+const categoryNames = Object.keys(CATEGORIES) as [keyof CategoriesMap, ...(keyof CategoriesMap)[]]
+export const CategoryEnum = z.enum(categoryNames)
 
-const allSubcategoryNames = Object.values(CATEGORIES).flat() as string[];
+const allSubcategoryNames = Object.values(CATEGORIES).flat() as string[]
 
 // If there are no subcategories defined at all, provide a fallback for z.enum
-export const AllSubcategoriesEnum = allSubcategoryNames.length > 0
-  ? z.enum(allSubcategoryNames as [string, ...string[]]) // Ensure at least one element for z.enum
-  : z.never(); // If no subcategories exist anywhere, this field should never be valid
+export const AllSubcategoriesEnum =
+	allSubcategoryNames.length > 0
+		? z.enum(allSubcategoryNames as [string, ...string[]]) // Ensure at least one element for z.enum
+		: z.never() // If no subcategories exist anywhere, this field should never be valid
